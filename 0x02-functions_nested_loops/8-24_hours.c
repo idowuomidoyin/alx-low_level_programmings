830d1e990e09745128a19ec591e56e9df8b773d5
idowu <idowuomidoyin@yahoo.com>
2023-02-21 20:22:47 -0800

    japoooo

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+{
+	int hour, minute;
+	{
+	for (hour = 0; hour <= 23; hour++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((minute / 10) + '0');
+		_putchar((minute % 10) + '0');
+		_putchar('\n')
+	}
+	}
+}
